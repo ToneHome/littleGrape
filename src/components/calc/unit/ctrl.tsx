2b5ee661fcff78295f8c4afe44ab32dc6d9ab5cd
ToneLee <297229267@qq.com>
2021-07-18 22:22:54 +0800

    完善计算器

--- src/components/calc/unit/ctrl.tsx
+++ src/components/calc/unit/ctrl.tsx
@@ -1,26 +1,35 @@
 import React, { Component } from 'react'
 
 export default class Ctrl extends Component {
+    constructor(){
+        super()
+        this.state = {
+            calcCtrl:['C','+','-','.','7','8','9','X','4','5','6','/','1','2','3','0','=']
+        }
+    }
+
+    doClick(item){
+        console.log(item);
+    }
+    mouseDown(e){
+
+    }
     render() {
+        const { calcCtrl } = this.state;
         return (
             <div className="ctrl-panel">
-                <div className="ctrl-item">C</div>
-                <div className="ctrl-item">+</div>
-                <div className="ctrl-item">-</div>
-                <div className="ctrl-item">.</div>
-                <div className="ctrl-item">7</div>
-                <div className="ctrl-item">8</div>
-                <div className="ctrl-item">9</div>
-                <div className="ctrl-item">X</div>
-                <div className="ctrl-item">4</div>
-                <div className="ctrl-item">5</div>
-                <div className="ctrl-item">6</div>
-                <div className="ctrl-item">/</div>
-                <div className="ctrl-item">1</div>
-                <div className="ctrl-item">2</div>
-                <div className="ctrl-item">3</div>
-                <div className="ctrl-item">0</div>
-                <div className="ctrl-item">=</div>
+                {
+                    calcCtrl.map((item,index) => {
+                        return (
+                             <div className="ctrl-item"
+                               key={index}
+                               onClick={this.doClick.bind(this,item)}
+                               onMouseDown={(e)=>this.mouseDown(e)}
+                             >{item}</div>
+                        )
+                    })
+                }
+
             </div>
         )
     }
